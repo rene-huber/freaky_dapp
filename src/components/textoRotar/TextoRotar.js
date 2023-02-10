ed03b4168fdde1b87c03113a67c2dd2a94949fad
Rene Huber <huberskull@gmail.com>
2023-02-10 18:43:35 +0100

    friday

--- src/components/textoRotar/TextoRotar.js
+++ src/components/textoRotar/TextoRotar.js
@@ -6,16 +6,25 @@ const TextoRotar = () => {
   const x = useMotionValue(0);
   const width = useMotionValue("3000px");
   
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 }
+  }
 
   return (
-    <div style={{overflow: "hidden"}}>
+    <div style={{overflow: "hidden", padding:"3px", display:"flex"}}>
+  <a href="https://www.youtube.com/watch?v=_Ln0xLQiKKc&ab_channel=efwewefg" target="_blank" rel="noopener noreferrer">
+    <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0b5493894cf60b300587_full_logo_white_RGB.svg"
+     alt="discord" height={"13px"} style={{color: "#FFF", position:"absolute", cursor:"pointer",
+     paddingTop:"4px"}} />
+     </a>
     <motion.div
       style={{
         
         
         background:"transparent",
         width: "1900px",
-        height: "70px",
+        height: "19px",
         overflow: "hidden",
         display: "flex",
         alignItems: "center",
@@ -24,12 +33,15 @@ const TextoRotar = () => {
     >
       <motion.p className="letrero"
         style={{ x,  width}}
-        initial={{ x: "100vw" }}
+        initial={{ x: "50vw" }}
       animate={{ x: "-100vw"  }}
-        transition={{ duration: 9, repeat: Infinity, }}
+        transition={{ duration: 7, repeat: Infinity, default: { ease: "linear" }}}
       >
-        NFT - SMART CONTRACT - W3 
+       &darr; WELCOME MEMARKET PLACE &darr;
       </motion.p>
+    
+     
+
      
     </motion.div>
     </div>
