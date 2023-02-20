0093c78963a7b5c8d999af345453b485f33cad90
Rene Huber <huberskull@gmail.com>
2023-02-20 23:45:03 +0100

    bike bg

--- src/components/astronauta/Astronauta.js
+++ src/components/astronauta/Astronauta.js
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import "../../global.css";
 import NextMint from '../nextMint/NextMint';
+import text from "../../img/bike-text.png"
 
 
 function Astronauta() {
   return (
     <div className='astronauta' >
-  
 
-    
-    <NextMint />
+  <NextMint /> 
 
    </div>
   )
--- src/components/nextMint/NextMint.js
+++ src/components/nextMint/NextMint.js
@@ -1,26 +1,19 @@
 import "../../global.css";
-import white from "./whitelist.png";
 
 function NextMint() {
+  const handleClick = () => {
+    window.open("https://rene-huber.eu/contact", "_blank");
+  };
+
   return (
-    <div className="nextMint">
-      <div className="white">
-        <p>
-          Get ready the next collection is incoming! you'll be able to ride in
-          style like never before. Don't miss out on this chance to add a rare
-          and exclusive piece (ChatGTP).
-        </p>
-        <div className="logoWhite">
-        <a href="http://" target="_blank" rel="noopener noreferrer" >
-          <img src={white} alt={"white list"}  />
-        </a></div>
+    <>
+      <div onClick={handleClick} className="nextMint" />
+      <div className="texNext">
+        Launch Announcement:  date and time on your website and social
+        media channels. Consider using email newsletters or paid ads to reach a
+        wider audience.
       </div>
-      {/* <div className="extraTexto"> 
-      Get ready the next collection is incoming! you'll be able to ride in
-      like never before. Don't miss out on this chance to add a rare
-          and exclusive piece Art (ChatGTP).
-        </div> */}
-    </div>
+    </>
   );
 }
 
